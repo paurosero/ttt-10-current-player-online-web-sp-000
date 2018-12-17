96df53ba95e05783f0ae9f815f7edb299c6f8fb1
Paul Rosero <paul_rosero@yahoo.com>
2018-12-17 21:10:26 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,8 +1,9 @@
 def turn_count(board)
 board_array = [" "," "," "," "," "," "," "," "," "]
 counter = 0
-board_array.each do |board_array| 
+board_array.each do |board_array|
 counter += 1
+ end
 end
 
 def current_player(board)
